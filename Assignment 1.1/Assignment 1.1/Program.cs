225650cb74dc3a7bd3d4d1bcfbded411d343ef87
vilgeralorraine <165258193+vilgeralorraine@users.noreply.github.com>
2024-03-29 14:31:28 +0800

    Initial commit

---
+++ Assignment 1.1/Assignment 1.1/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.ComponentModel;
+
+namespace Assignment
+{
+    class Program
+    {
+        static void Main(string[] args) 
+        {
+            int attempt = 3;
+            while (attempt > 0)
+            {
+                Console.Write("Type your Username: ");
+                string Username = Console.ReadLine();
+
+                Console.Write("Type your Password: ");
+                string Password = Console.ReadLine();
+
+                if (Username == "Admin" && Password == "Admin1234!")
+                {
+                    Console.WriteLine("Login successful!");
+                    break;
+                }
+                else
+                {
+                    attempt--;
+                    Console.Clear();//
+                    Console.WriteLine("Incorrect Credentials! Remaining attempts:" + attempt);
+                }
+            }
+
+            if (attempt == 0)
+            {
+                Console.WriteLine("You have run out of attempts! Access denied.");
+            }
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,2 @@
+# C--Activities
+ 
